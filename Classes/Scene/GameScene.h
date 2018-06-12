6a4b3fad5a55a7d54291bbe07e1e05f4c92d7daa
the father <2116726984@qq.com>
2018-06-12 21:44:57 +0800

    添加移动地图初步功能

--- Classes/Scene/GameScene.h
+++ Classes/Scene/GameScene.h
@@ -7,7 +7,16 @@ USING_NS_CC;
 using namespace std;
 enum
 {
-	BUTTON_AMOUNT = 0
+	ITEM_AMOUNT = 0
+};
+enum
+{
+	MAP_LAYER_TAG,
+	MENU_LAYER_TAG
+};
+const string itemImageAdress[ITEM_AMOUNT + 1][3]
+{
+	"hello.png","hello.png","hello.png"
 };
 /*这个场景为游戏场景，分为三层：地图，菜单层，文本层*/
 
@@ -40,12 +49,20 @@ class Game:public Scene
 private:
 	TMXTiledMap * _tileMap;
 	TMXLayer * _collidable;
+	//static auto visibleSize = Director::getInstance()->getVisibleSize();
+	//static Vec2 origin = Director::getInstance()->getVisibleOrigin();
 public:
 	virtual bool init();
 	void initMap();
-	void initMenu();
-	static Game* createScece();
+	//void initMenu();
+	static Game* createScene();
 	CREATE_FUNC(Game);
+	void onEnter();
+	void onExit();
+
+	bool clickDown(Event *event);
+	void clickMove(Event *event);
+	void clickUp(Event *event);
 };
 
 
