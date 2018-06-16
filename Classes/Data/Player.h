cf45369cab7aa66e4a82ce5b7bccaabdd8107f28
the son <linpx2017@outlook.com>
2018-06-16 17:57:47 +0800

    changed file

--- Classes/Data/Player.h
+++ Classes/Data/Player.h
@@ -7,11 +7,11 @@
 #include<string>
 #include<iostream>
 using namespace std;
-/*Player类，继承自Node，记录玩家数据*/
 
-class Player: public cocos2d::Sprite
+class Player : public cocos2d::Sprite
 {
 private:
+	Sprite* id = Sprite::create("soldier.png");
 	CC_SYNTHESIZE(int, inithp, _inithp);       //初始血量
 	CC_SYNTHESIZE(int, hp, _hp);               //现在血量
 	CC_SYNTHESIZE(float, vel, _vel);           //初始速度
@@ -19,59 +19,21 @@ private:
 	bool _alreadyDead = false;                 //玩家是否真正的死亡
 public:
 	Player();
-	Player(int inithp)
-	{
-		this->inithp = 100;
-	}
-	void setSpeed(float speed)
-	{
-		vel = speed;
-	}
 
-	float getSpeed()
-	{
-		return vel;
-	}
+	Player(int inithp);
 
-	static Player* create(const std::string& filename)
-	{
-		Player *sprite = new Player();
-		if (sprite && sprite->initWithFile(filename))
-		{
-			sprite->autorelease();
-/*			sprite->setVisible(false);
+	static Player* createWithSpriteFrameName(const char * SpriteFrameName);
+	
+	void Birthplace(Arch *arch);    //设置人物初始位置
 
-			auto body = PhysicsBody::createBox(sprite->getContentSize());
+	void setPlayerDead();           //设置人物死亡
+	
+	bool isDead();                 //判断人物是否死亡
+	
 
-			body->setCategoryBitmask(0x01);
-			body->setCollisionBitmask(0x02);
-			body->setContactTestBitmask(0x01);
+	void moveToPosition(cocos2d::Vec2 & position);
 
-			sprite->setPhysicsBody(body);
-*/
-			return sprite;
-		}
-		CC_SAFE_DELETE(sprite);
-		return nullptr;
-	}
-/*	void Birthplace(Arch *Arch)
-	{
-		this->setPosition(arch->getPosition() + cocos2d::Vec2(0, arch->getContentSize().height / 2));
-		this->setVisible(true);
-	}
-*/
-	void setPlayerDead()           //设置人物死亡
-	{
-		_isDead = true;
-	}
-
-	bool isDead()                 //判断人物是否死亡
-	{
-		return _isDead;
-	}
-
-
-
+     virtual void update(float dt);
 
 };
 
